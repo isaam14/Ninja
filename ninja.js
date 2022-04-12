334bcbf8d030c1477588174cafbc6e1cf052cbe1
isaam14 <isaam14@yahoo.fr>
2022-04-11 23:41:30 -0500

    [TAREA] sensei

--- ninja.js
+++ ninja.js
@@ -19,9 +19,30 @@ class Ninja{
     }
 }
 
-
 const ninja1 = new Ninja("Hyabusa",4);
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
 ninja1.showStats();
+
+
+class Sensei extends Ninja{
+    constructor(nombre){
+        super(nombre,200);
+        this.velocidad=10;
+        this.fuerza=10;
+        this.sabiduria=10;
+    }
+
+    speakWisdom(){
+        super.drinkSake();
+        console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.");
+    }
+}
+// ejemplo de salida
+console.log("------------------------");
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses."
+superSensei.showStats();
+// -> "Nombre: Master Splinter, Salud: 210, Velocidad: 10, Fuerza: 10"
